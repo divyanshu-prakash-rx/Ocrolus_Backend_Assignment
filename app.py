eabd4389204c66ba425c36f46bc18bcd86a471a1
Divyanshu Prakash <medivyanshu780@gmail.com>
2025-07-10 04:00:05 +0530

    Added Comments

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ class Article(db.Model):
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
     
+    # Funtion to return articles information as a diction
     def to_dict(self):
         return {
             'id': self.id,
